96d477a6ba9e9a0a0fb1e5b0940c7952ab45c985
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-14 02:58:59 +0500

    add

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
-<h1>Hello</h1>
+<h1>Hello 22</h1>
 <Header/>
 {/* header */}
 {/* SIDEBAR*/}
--- src/Header.js
+++ src/Header.js
@@ -1,28 +1,37 @@
 import React from "react";
-import SearchIcon from '@material-ui/icons/Search';
-import FlagIcon from '@material-ui/icons/Flag';
-import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from "@material-ui/icons/Search";
+import FlagIcon from "@material-ui/icons/Flag";
+import HomeIcon from "@material-ui/icons/Home";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/Subscriptions";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
 import "./Header.css";
 function Header() {
   return (
     <div className="header">
       <div className="header__left">
-      <img src="https://i.ibb.co/XSbCXmk/fb.png" alt="fblogo"/>  
-      <div className="header__input">
-      <SearchIcon/>
-      <input type="text"/>
-      </div>  
+        <img src="https://i.ibb.co/XSbCXmk/fb.png" alt="fblogo" />
+        <div className="header__input">
+          <SearchIcon />
+          <input type="text" />
+        </div>
       </div>
       <div className="header__middle">
-   
-       <div className="header__option">
-       <HomeIcon fontSize="large" />
-
-       </div>
-       <div className="header__option">
-       <FlagIcon fontSize="large" />
-       
-       </div>
+        <div className="header__option">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
       </div>
       <div className="header__right"></div>
     </div>
