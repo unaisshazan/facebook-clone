63c82e9c48f1ec8766d9e47e1ea18a33c30f47cb
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-13 18:50:52 +0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,8 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header">
-      <div className="header_left"></div>
+      <div className="header_left">  
+        </div>
       <div className="header_middle"></div>
       <div className="header_right"></div>
     </div>
