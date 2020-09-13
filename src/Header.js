dda9e83e49409b12ed2a6578e9dfc28398c9d755
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-14 03:37:03 +0500

    add css

--- src/Header.js
+++ src/Header.js
@@ -21,7 +21,7 @@ function Header() {
           <input type="text" />
         </div>
       </div>
-      <div className="header__middle">
+      <div className="header__center">
         <div className="header__option">
           <HomeIcon fontSize="large" />
         </div>
