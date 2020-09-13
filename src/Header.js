96d9465ecffcdb0d588833bdb799410e7fa93865
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-13 19:05:52 +0500

    material ui

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import SearchIcon from '@material-ui/icons/Search';
 import "./Header.css";
 function Header() {
   return (
@@ -7,7 +7,9 @@ function Header() {
       <div className="header__left">
       <img src="https://i.ibb.co/XSbCXmk/fb.png" alt="fblogo"/>  
         </div>
-      <div className="header__middle"></div>
+      <div className="header__middle">
+      <SearchIcon/>
+      </div>
       <div className="header__right"></div>
     </div>
   );
