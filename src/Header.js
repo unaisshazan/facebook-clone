5fe045d4784f611d3ca39455a268e667becaf423
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-13 18:43:47 +0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,14 @@
- import React from 'react'
+import React from "react";
 
- import "./Header.css"
- function Header() {
-     return (
-         <div className="header">
-             
-         </div>
-     )
- }
- 
- export default Header
- 
+import "./Header.css";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header_left"></div>
+      <div className="header_middle"></div>
+      <div className="header_right"></div>
+    </div>
+  );
+}
+
+export default Header;
