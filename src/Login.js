55bb56b5dcaf0c122d8c4772f8a2076da9efffc7
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-15 00:55:42 +0500

    add

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,13 @@
 import { Button } from "@material-ui/core";
 import React from "react";
 import "./Login.css";
+import {auth,provider} from "./firebase"
 function Login() {
     const signIn =()=>{
+auth.signInWithPopup(provider)
+.then(result=>{
+    console.log(result.user)
+}).catch(error=> alert(error.message))
 
     };
   return (
