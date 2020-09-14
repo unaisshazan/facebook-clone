806f9f761a9b870c1e53241ea2719979ab3bf46f
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-14 05:47:07 +0500

    add

--- src/Story.js
+++ src/Story.js
@@ -6,7 +6,7 @@ import './Story.css'
 function Story({image,profileSrc,title}) {
     return (
         <div style={{backgroundImage:`url(${image})`}} className="story">
-            <Avatar src={profileSrc}/>
+            <Avatar className="story__avatar" src={profileSrc}/>
             <h4>{title}</h4>
         </div>
     )
