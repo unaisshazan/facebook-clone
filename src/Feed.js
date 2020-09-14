94969832063721e3e2b4dc8a5129512994d9485f
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-15 01:27:26 +0500

    Update Feed.js

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Feed.css'
 import StoryReel from './StoryReel'
 import MessageSender from './MessageSender'
 import Post from './Post'
+import db from "./firebase"
 const Feed = () => {
+const [posts,setPosts]=useState([]);
+useEffect(() => {
+  db.collection('posts').onSnapshot(snapshot=>(
+    setPosts(snapshot.docs.map(doc=>({
+      id:doc.id , data: doc.data()
+    })))
+  ))
+ 
+}, []);
     return (
         <div className="Feed">
         <StoryReel />    
