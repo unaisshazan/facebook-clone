8e99cbc5a2baab90e1976235c830b4f134d09262
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-14 14:29:24 +0500

    Update MessageSender.js

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import './MessageSender.css'
 function MessageSender() {
+    const handleSubmit=e=>{
+
+      e.preventDefault();  
+    }
     return (
         <div className='messageSender'>
+        <div className="messageSender__top">
+        <Avatar />
+        <form >
+            <input placeholder={`Whats in your mind`}/>
+            <input placeholder="image URL (Optional)"/>
+             <button onClick={handleSubmit} type="submit">
+             Hidden submit</button>       
+            </form>
+        </div>
+        <div className="messageSender__bottom">
+        </div>
             
         </div>
     )
