415580decac8f367ebcb6b1aa490715c0cb7257e
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-14 14:40:40 +0500

    Update MessageSender.js

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './MessageSender.css'
 import {Avatar} from '@material-ui/core';
-
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import VideocamIcon from '@material-ui/icons/Videocam';
 function MessageSender() {
     const handleSubmit=e=>{
 
@@ -19,6 +21,18 @@ function MessageSender() {
             </form>
         </div>
         <div className="messageSender__bottom">
+        <div className="messsageSender__option">
+        <VideocamIcon style={{color:"red"}} />
+        <h3>Live Video</h3>
+        </div>
+        <div className="messsageSender__option">
+        <PhotoLibraryIcon style={{color:"green"}} />
+        <h3>Photo/Video</h3>
+        </div>
+        <div className="messsageSender__option">
+        <InsertEmoticonIcon  style={{color:"orange"}} />
+        <h3>Feelings/Activity</h3>
+        </div>
         </div>
             
         </div>
