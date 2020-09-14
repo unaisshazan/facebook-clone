c02617e645e369cfa0b5ac78cef2bbf29a88e51c
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-14 15:58:37 +0500

    Update Feed.js

--- src/Feed.js
+++ src/Feed.js
@@ -8,8 +8,18 @@ const Feed = () => {
         <div className="Feed">
         <StoryReel />    
           <MessageSender/>
-          <Post />
-          <Post />
+          <Post profilePic="https://i.ibb.co/9WBk6LS/Whats-App-Image-2020-06-12-at-10-53-53-PM.jpg"
+          message='wow i am certfied'
+          timestamp="This is time stamp"
+          username="Talha"
+          image="https://i.ibb.co/k6V9hRk/image1.jpg"
+          
+          />
+          <Post profilePic="https://i.ibb.co/9WBk6LS/Whats-App-Image-2020-06-12-at-10-53-53-PM.jpg"
+          message='wow i am certfied'
+          timestamp="This is time stamp"
+          username="Amal"
+          image="https://i.ibb.co/k6V9hRk/image1.jpg"/>
           <Post />
 
         </div>
