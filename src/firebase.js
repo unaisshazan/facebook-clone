c19b37786a7a7f0ef33454590b347fda0dcd83c3
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-15 00:31:18 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -15,3 +15,5 @@ const firebaseConfig = {
   const db =firebaseApp.firestore();
   const auth =firebase.auth();
   const provider = new firebase.auth.GoogleAuthProvider();
+  export {auth, provider}
+  export default db;
